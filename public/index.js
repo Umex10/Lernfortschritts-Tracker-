f294f476cd7be0075c1afb100c9dff4bb2504338
Rozaliya Aleksandrova <rozaliya.aleksandrova@edu.fh-joanneum.at>
2026-01-19 18:04:09 +0100

    Implement module display and global function registration

--- public/index.js
+++ public/index.js
@@ -1,4 +1,6 @@
 import { getModules } from "./getModules.js";
+import {changeModuleStatus} from "./statusFilter.js";
+
 
 const reload = document.getElementById("reload");
 const taskList = document.getElementById("tasks");
@@ -8,12 +10,25 @@ export const setTasks = (newModules) => {
 
   newModules.forEach(module => {
     const li = document.createElement("li");
-    li.textContent = `${module.id} – ${module.status}`;
+    li.className = "module-item";
+    li.innerHTML = `
+    <strong>${module.title}</strong>(ID: ${module.id})
+    <span class = "status">${module.status}</span>
+    <button onclick="changeModuleStatus(${module.id}, 'todo')">To do</button>
+    <button onclick="changeModuleStatus(${module.id}, 'in-progress')">In Progress</button>
+    <button onclick="changeModuleStatus(${module.id}, 'done')">Done</button>
+    `;
     taskList.appendChild(li);
   });
 };
+window.setTasks = setTasks; 
+windows.getModules = getModules;
 
-
+document.addEventListener("DOMContentLoaded", () => {
+  const modules = getModules();
+  if (modules){setTasks(modules);
+  }
+});
 
 reload.addEventListener("click", () => {
   console.log("Reloaded")
